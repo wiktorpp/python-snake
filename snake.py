9d0cb5f170055db5029469406d809cfa17eff130
wiktorpp <grazynapp65@gmail.com>
2021-07-05 11:05:05 +0200

    First ccommit

---
+++ snake.py
@@ -0,0 +1,14 @@
+class Snake:
+    def __init__(self, PlaneType="finite", boardSize=(8, 19)):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def tick():
